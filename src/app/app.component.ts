d30325b7b9f2d4797a05eab40d2844fff65ae557
kammounmustapha <37082830+kammounmustapha@users.noreply.github.com>
2019-02-21 14:56:30 +0100

    Add GamePlay Service Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { GamePlayService } from './game-play.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GamePlayService]
 })
 export class AppComponent {
   title = 'Tic Tac Toe Game';
+
+  constructor(gps: GamePlayService) {
+
+    
+  }
 }
  
---
+++ src/app/game-play.service.ts
@@ -0,0 +1,42 @@
+import { Player } from './player';
+import { Block } from './block';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GamePlayService {
+  players = []
+  turn: number = 0;
+  draw: number = 0;
+
+  blocks = []
+  freeRemainingBlocks = 9;
+
+  constructor() {
+    this.initBlocks();
+    this.initPlayers();
+   }
+  
+  initBlocks() {
+    this.blocks = []
+    for (var i=1; i<=9; i++) {
+       var block = new Block(); 
+       block.free = true;
+       block.value = "";
+       block.symbol = "";
+       
+       this.blocks.push(block); 
+    }
+  }
+
+  initPlayers() {
+    this.players = [];
+    var player1 = new Player();
+    player1.bot = false;
+
+    var player2 = new Player();
+    this.players.push(player1);
+    this.players.push(player2);
+  }
+  
+
+}
